440ffc24a63c9a217cba8e42caf3851dfe966b52
Ángela García <97111035+garciahuelamo@users.noreply.github.com>
2024-01-28 16:51:02 +0100

    Version 28.01.24

---
+++ Práctica/Proyecto - POO/Practica5 - POO/Base de datos - phpMyAdmin/Remolques/002-Insertar remolques.sql
@@ -0,0 +1,2 @@
+INSERT INTO `remolques` (`id`, `matricula_remolque`, `tipo`, `fecha_matriculacion`, `ITV`) 
+VALUES (NULL, 'HROB5479', 'MEGA', '2024-01-02', '2026-03-19')
---
+++ Práctica/Proyecto - POO/Practica6 - POO/Base de datos - phpMyAdmin/001-Crear base de datos.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE avp_enterprise;
